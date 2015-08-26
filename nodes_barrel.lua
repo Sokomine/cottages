b300e9e4b0bbbf3755e5ec6f05caca870fc8e4e6
Sokomine <wegwerf@anarres.dyndns.org>
2015-08-26 05:43:03 +0200

    added collision box for the tub

--- nodes_barrel.lua
+++ nodes_barrel.lua
@@ -178,6 +178,11 @@ end
 			fixed = {
 				{-0.5, -0.5, -0.5, 0.5,-0.1, 0.5},
 			}},
+		collision_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, -0.5, -0.5, 0.5,-0.1, 0.5},
+			}},
                 groups = { tree = 1, snappy = 1, choppy = 2, oddly_breakable_by_hand = 1, flammable = 2
                 },
 		is_ground_content = false,
