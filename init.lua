3027973c3a8cb749fb993f3a7dd2528c632a0a3f
Sokomine <wegwerf@anarres.dyndns.org>
2017-06-30 19:58:52 +0200

    added special ladder and rope for mines

--- init.lua
+++ init.lua
@@ -63,6 +63,7 @@ dofile(minetest.get_modpath("cottages").."/nodes_doorlike.lua");
 dofile(minetest.get_modpath("cottages").."/nodes_fences.lua");
 dofile(minetest.get_modpath("cottages").."/nodes_roof.lua");
 dofile(minetest.get_modpath("cottages").."/nodes_barrel.lua");
+dofile(minetest.get_modpath("cottages").."/nodes_mining.lua");
 --dofile(minetest.get_modpath("cottages").."/nodes_chests.lua");
 
 -- this is only required and useful if you run versions of the random_buildings mod where the nodes where defined inside that mod
